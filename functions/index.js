7882b8dd60c1a272e46af54a40631aa8211fef6b
Stefan Guelland <stefan@sguelland.de>
2018-06-10 16:06:16 +0200

    add logs to server functions
    Signed-off-by: Stefan Guelland <stefan@sguelland.de>

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,37 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+const moment = require('moment');
+
 admin.initializeApp();
 
 exports.lora = functions.https.onRequest((request, response) => {
     let serial = request.body.hardware_serial;
 
-    let nodeObject = {};
-    nodeObject["data"]  = request.body.payload_fields;
-    nodeObject["lastUpdate"] = request.body.metadata.time;
+    const dateTime = request.body.metadata.time;
+    const data = request.body.payload_fields;
 
-    const usersRef = admin.database().ref("nodes/" + serial);
+    const dataRef = admin.database().ref("nodes/" + serial + "/data");
+    const nodeDataPromise = dataRef.set(data);
+
+    const timeRef = admin.database().ref("nodes/" + serial + "/lastUpdate");
+    const nodeTimePromise = timeRef.set(dateTime);
 
     response.end();
 
-    return usersRef.set(nodeObject);
+    const logObject = {
+        time: dateTime,
+        data: data
+    };
+
+    const date = moment(dateTime).format('YYYY-MM-DD');
+    const time = moment(dateTime).format('HH-mm-ss-SSS');
+    const logRef = admin.database().ref("logs/" + serial + "/" + date + "/" + time);
+    const logPromise = logRef.set(logObject);
+
+    return Promise.all([
+        nodeDataPromise,
+        nodeTimePromise,
+        logPromise
+    ]);
 });
